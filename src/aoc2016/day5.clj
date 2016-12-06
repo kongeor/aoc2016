af26b81caf80cf2174705b11a725a1da6dbfee0e
Kostas Georgiadis <kongeor@gmail.com>
2016-12-06 20:42:58 +0200

    Implement day5 part2

--- src/aoc2016/day5.clj
+++ src/aoc2016/day5.clj
@@ -21,4 +21,37 @@
         pass
         (recur (inc i) pass)))))
 
-(break-password "ugkcyxxp")
+(comment
+  (break-password "ugkcyxxp"))
+
+(defn solved? [pass]
+  (= (count (filter nil? pass)) 0))
+
+(defn char->int [c]
+  (try
+    (Integer/parseInt (str c))
+    (catch NumberFormatException e)))
+
+(char->int \1)
+
+(defn update-pass [pass c idx]
+  (if-let [idx (char->int idx)]
+    (if (< -1 idx 8)
+      (assoc pass (char->int idx) (str c))
+      pass)
+    pass))
+
+(update-pass [nil nil nil] \c \1)
+
+(defn break-password-2 [id]
+  (loop [i 1
+         pass (vec (repeat 8 nil))]
+    (let [[hash i] (digest-id id i)
+          [c idx] (. hash substring 5 8)
+          pass (update-pass pass c idx)]
+      (if (solved? pass)
+        pass
+        (recur (inc i) pass)))))
+
+(comment
+  (break-password-2 "ugkcyxxp"))
